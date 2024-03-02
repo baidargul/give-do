8efc2ad48142005bbaeacbc026d815b205c97958
Baidar Gul <baidargul@outlook.com>
2024-03-03 00:53:01 +0500

    divided amoun log into components

--- app/(routes)/fundraisers/components/DonateSection/Section.tsx
+++ app/(routes)/fundraisers/components/DonateSection/Section.tsx
@@ -1,5 +1,5 @@
-import { Progress } from '@/components/ui/progress'
 import React from 'react'
+import AmountLog from './components/AmountLog'
 
 type Props = {}
 
@@ -7,39 +7,7 @@ const DonateSection = (props: Props) => {
     return (
         <div className='p-[32px] border drop-shadow-lg bg-white rounded-2xl w-full'>
             <div>
-                <Progress value={12} />
-                <div className='flex justify-between items-center mt-4'>
-                    <div className='w-[60%]'>
-                        <div className='text-xs'>
-                            <div className='text-lg tracking-wide'>
-                                $18,496
-                            </div>
-                            <div className='flex gap-1 items-center'>
-                                <span>Raised of</span>
-                                <span>$60.3K goal</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-[40%] flex items-center'>
-                        <div className='text-xs'>
-                            <div className='text-lg tracking-wide'>
-                                947
-                            </div>
-                            <div className='flex gap-1 items-center'>
-                                <span>Supporters</span>
-                            </div>
-                        </div>
-                        <div className='text-xs'>
-                            <div className='text-lg tracking-wide'>
-                                4
-                            </div>
-                            <div className='flex gap-1 items-center'>
-                                <span>Days Left</span>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <AmountLog/>
             </div>
         </div>
     )
---
+++ app/(routes)/fundraisers/components/DonateSection/components/AmountLog.tsx
@@ -0,0 +1,46 @@
+import { Progress } from '@/components/ui/progress'
+import React from 'react'
+
+type Props = {}
+
+const AmountLog = (props: Props) => {
+    return (
+        <div>
+            <Progress value={12} />
+            <div className='flex justify-between items-center mt-4'>
+                <div className='w-[60%]'>
+                    <div className='text-xs'>
+                        <div className='text-lg tracking-wide'>
+                            $18,496
+                        </div>
+                        <div className='flex gap-1 items-center'>
+                            <span>Raised of</span>
+                            <span>$60.3K goal</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-[40%] flex items-center gap-4'>
+                    <div className='text-xs'>
+                        <div className='text-lg tracking-wide'>
+                            947
+                        </div>
+                        <div className='flex gap-1 items-center'>
+                            <span>Supporters</span>
+                        </div>
+                    </div>
+                    <div className='text-xs'>
+                        <div className='text-lg tracking-wide'>
+                            4
+                        </div>
+                        <div className='flex gap-1 items-center'>
+                            <span>Days Left</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AmountLog
