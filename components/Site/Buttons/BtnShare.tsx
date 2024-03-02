5ebc6a524aa2fb21777bf8bd6a9e25f1df5f435e
Baidar Gul <baidargul@outlook.com>
2024-03-03 01:25:37 +0500

    working on button animation

--- components/Site/Buttons/BtnShare.tsx
+++ components/Site/Buttons/BtnShare.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useRef } from 'react'
 
 type Props = {}
 
 const BtnShare = (props: Props) => {
+    const buttonRef: any = useRef();
+
+    useEffect(() => {
+        const applyContainerProperties = () => {
+            buttonRef.current?.classList.add("effect-container")
+        }
+        applyContainerProperties()
+
+        const onClick = () => {
+            buttonRef.current?.classList.remove("active")
+            buttonRef.current?.classList.add("active")
+        }
+
+        buttonRef.current?.addEventListener("mouseup", onClick)
+
+
+        return () => {
+            buttonRef.current?.removeEventListener("mouseup", onClick)
+        }
+
+    }, [])
+
     return (
-        <button className='text-sm text-site-button-share-text min-w-[150px] h-[44px] hover:text-site-button-share-text_hover font-semibold bg-white rounded-md  border border-site-button-share-border drop-shadow-sm'>
-            Share
-        </button>
+        <div ref={buttonRef}>
+            <button className='text-sm text-site-button-share-text min-w-[150px] h-[44px] hover:text-site-button-share-text_hover font-semibold bg-white rounded-md  border border-site-button-share-border drop-shadow-sm'>
+                Share
+            </button>
+        </div>
     )
 }
 
