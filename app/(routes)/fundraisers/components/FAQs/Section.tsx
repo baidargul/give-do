58ac2e5fae3a7f4d68ed54eb589a3a329ffef4ca
Baidar Gul <baidargul@outlook.com>
2024-03-03 20:37:49 +0500

    added questions to FAQ

--- app/(routes)/fundraisers/components/FAQs/Section.tsx
+++ app/(routes)/fundraisers/components/FAQs/Section.tsx
@@ -1,25 +1,23 @@
-import Accordion from '@/components/Site/Accordion/Accordion'
-import LinkButton from '@/components/Site/Buttons/LinkButton'
 import React from 'react'
+import Questions from './Questions'
 
 type Props = {}
 
 const FAQSection = (props: Props) => {
     return (
-        <div className='px-40 pt-2'>
+        <div className='px-40 pt-2 flex justify-between gap-5'>
 
             <div className='w-[40%]'>
                 <div className="text-[32px] my-2 font-semibold">FAQs</div>
                 <div>
                     <div className=''>Everything you need to know about the fundraiser and your donation. Can’t find the answer you are looking for?
-                        <span className='text-site-bg-redHighlight hover:underline'> Please chat with our friendly team</span>
+                        <span className='text-site-bg-redHighlight underline'> Please chat with our friendly team</span>
                     </div>
                 </div>
             </div>
 
             <div className='w-[60%]'>
-                <Accordion />
-                This
+                <Questions />
             </div>
         </div>
     )
--- components/Site/Accordion/Accordion.tsx
+++ components/Site/Accordion/Accordion.tsx
@@ -1,12 +1,28 @@
 'use client'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-type Props = {}
+type Props = {
+    isSelected?: boolean
+    title: string
+    content: string
+}
 
 const Accordion = (props: Props) => {
     const [isToggled, setIsToggled] = useState(false)
     const [togglePadding, setTogglePadding] = useState(false)
+    const [selected, setSelected] = useState<boolean>(false)
+
+    useEffect(() => {
+        setSelected(props.isSelected ? true : false)
+    }, [props.isSelected])
+
+    useEffect(() => {
+        if (selected === false) {
+            setTogglePadding(false)
+            setIsToggled(false)
+        }
+    }, [selected])
 
     const handleToggle = () => {
         setIsToggled(!isToggled)
@@ -21,22 +37,22 @@ const Accordion = (props: Props) => {
     }
 
     return (
-        <div className='bg-site-bg-accordion p-8 rounded-2xl text-site-text-tag_first_donor text-[1.125rem] font-semibold cursor-pointer'>
-            <div onClick={handleToggle} className='flex justify-between items-center'>
-                <div>
-                    How does Give.do Fundraisers work?
+        <button onClick={handleToggle} className='bg-site-bg-accordion transition-all duration-500 p-8 rounded-2xl text-site-text-tag_first_donor text-[1.125rem] font-semibold cursor-pointer'>
+            <div className='flex justify-between items-center transition-all duration-500'>
+                <div className='text-start'>
+                    {props.title}
                 </div>
                 <div>
                     <Image src={isToggled ? "/other/faq_minusIcon.png" : "/other/faq_plusIcon.png"} width={20} height={20} alt='faq' className='opacity-50' />
                 </div>
             </div>
             <div className={`transition-all duration-500 pt-0 h-0 overflow-hidden ${isToggled ? 'h-auto' : 'h-0'} ${togglePadding && "pt-0"}`}>
-                <p className={`text-sm leading-relaxed font-normal pt-4 ${togglePadding && "pt-0"} transition-all duration-500`}>
-                    Yes, all online transactions on Give.do Fundraisers are secure. All of our platforms and APIs use TLS encryption with HSTS and Forward Secrecy. Our payment processing partners Razorpay and Stripe have been certified to PCI DSS which is the most stringent level of certification available in the payments industry.
+                <p className={`text-sm transition-all duration-500 text-start leading-relaxed font-normal pt-4 ${togglePadding && "pt-0"} transition-all duration-500`}>
+                    {props.content}
                 </p>
             </div>
 
-        </div>
+        </button>
     )
 }
 
