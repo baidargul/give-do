9da6a024d98367056390210ee2cfa96800549970
Baidar Gul <baidargul@outlook.com>
2024-03-03 12:21:52 +0500

    fixed tailwind bug

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',],
+  theme: {
+    extend: {
+      colors: {
+        site: {
+          text: {
+            title: "#333333",
+            tax_benefits_available: "#D97613",
+            fundraiser_support_description: "#667085",
+            donation_section: "#363F72",
+          },
+          bg: {
+            tax_benefits_available: "#FDF2E2",
+            redHighlight: "#E7424B",
+          },
+          progressBar: {
+            bg: "#E8E8E8",
+            value: "#FBAF17"
+          },
+          section: {
+            bg: {
+              support_section: "#FDEEEE",
+              donation_section: "#EAF2FD",
+            }
+          },
+          button: {
+            share: {
+              border: "#D0D5DD",
+              text: "rgba(0, 0, 0, 0.87)",
+              text_hover: "#666666",
+            }
+          },
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
