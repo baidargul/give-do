14bb1ff2c2fc00695557e1e1a657e6f8c1a09ec5
Baidar Gul <baidargul@outlook.com>
2024-03-03 23:47:10 +0500

    alloting media queries device names

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,13 +82,13 @@ module.exports = {
       },
     },
     screens: {
-      'sm': '576px',
+      'phone': '576px',
       // => @media (min-width: 576px) { ... }
 
-      'md': '960px',
+      'tablet': '960px',
       // => @media (min-width: 960px) { ... }
 
-      'lg': '1440px',
+      'laptop': '1440px',
       // => @media (min-width: 1440px) { ... }
     },
   },
