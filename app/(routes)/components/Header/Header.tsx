3acb5798896bceb9846255991a98b75e87b452c5
Baidar Gul <baidargul@outlook.com>
2024-03-05 17:49:37 +0500

    Header UI Fix

--- app/(routes)/components/Header/Header.tsx
+++ app/(routes)/components/Header/Header.tsx
@@ -6,13 +6,13 @@ type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <div className='flex justify-between phone:mx-0 tablet:mx-10 laptop:mx-40 mb-5 py-2'>
+        <div className='flex justify-between items-center phone:mx-0 tablet:mx-10 laptop:mx-40 mb-2 tablet:mb-5 p-4 px-6 h-[74px] tablet:px-0 py-2'>
             <div>
                 <Image src={"/logo.png"} width={92.63} height={44} className='w-[92.63px] h-[44px]' alt='site-logo' />
             </div>
             <div className='flex justify-between gap-4 items-center'>
-                <div>Find Fundraisers</div>
-                <div><CurrencySelector /></div>
+                <div className='hidden tablet:block'>Find Fundraisers</div>
+                <div className='hidden tablet:block'><CurrencySelector /></div>
                 <div>Account</div>
             </div>
         </div>
