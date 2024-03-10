cb9cae296d19f7276f577d067d2e8a4ead74d9f5
Baidar Gul <baidargul@outlook.com>
2024-03-10 14:22:12 +0500

    added coursel controls

--- components/Site/Carousel/Homepage/HomepageCoursel.tsx
+++ components/Site/Carousel/Homepage/HomepageCoursel.tsx
@@ -46,21 +46,37 @@ const images = [
 export function HomepageCoursel() {
 
   return (
-    <Carousel className="w-full" opts={{loop: true, align:"start"}} plugins={[Autoplay({delay: 2000,}),]}>
-      <CarouselContent>
-        {images.map((image, index) => (
-          <CarouselItem key={index}>
-            <div className="w-full cursor-pointer select-none" onClick={()=>{}}>
-              <Image src={image.image} alt={image.alt} width={1466} height={490} className="w-full select-none pointer-events-none"/>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <div>
+    <div className="relative">
+      <Carousel className="w-full" opts={{ loop: true, align: "start" }} plugins={[Autoplay({ delay: 2000, }),]}>
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="w-full cursor-pointer select-none" onClick={() => { }}>
+                <Image src={image.image} alt={image.alt} width={1466} height={490} className="w-full select-none pointer-events-none" />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
 
-      </div>
-      {/* <CarouselPrevious /> */}
-      {/* <CarouselNext /> */}
-    </Carousel>
+
+        <div className="absolute bottom-3 w-full">
+          <div className="flex justify-center items-center">
+
+            <div className={`grid grid-cols-6 w-fit gap-4  justify-center items-center justify-items-center `}>
+              {
+                images.map((image, index) => {
+
+                  return (
+                    <div className="bg-site-text-fundraiser_support_description w-[32px] h-[5px] " key={index}>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+
+      </Carousel>
+    </div>
   )
 }
