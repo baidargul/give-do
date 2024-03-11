8a11d451d504fda2cb8681cbe8a7ca0e640f8bd9
Baidar Gul <baidargul@outlook.com>
2024-03-11 18:11:54 +0500

    added gradient to mission card

--- components/Site/Cards/Missions/MissionCard.tsx
+++ components/Site/Cards/Missions/MissionCard.tsx
@@ -21,11 +21,12 @@ const MissionCard = (props: Props) => {
             <div className=''>
                 <div className='relative'>
                     <Image src={`/homepage/missions/${props.mission.image}.jpg`} width={395.52} height={341.96} alt={`${props.mission.entity}`} className='rounded-2xl object-cover overflow-clip w-[384px] h-[332px]' />
-                    <div className='absolute flex flex-col bottom-2 left-4 text-white'>
+                    <div className='absolute flex flex-col bottom-2 left-4 text-white z-[1]'>
                         <span className='text-[16px] italic'>Mission</span>
                         <span className='text-[24px] font-bold'>{props.mission.title}</span>
                     </div>
                     <div className='text-[12px] font-semibold px-[12px] py-[4px] absolute top-3 bg-site-bg-tag_tax_benefits_available rounded-r-md text-site-text-tag_tax_benefits_available'>Tax Benefits Available</div>
+                    <div className='absolute bottom-0 bg-gradient-to-t from-black to-transparent h-20 w-full'></div>
                 </div>
             </div>
             <div className='h-[77.24px] bg-white  rounded-b-2xl'>
@@ -33,7 +34,7 @@ const MissionCard = (props: Props) => {
                     <BottomSection mission={props.mission} />
                 </div>
                 <div className='hidden group-hover:block p-4'>
-                    <BtnShare red caption='Pledge Monthly'/>
+                    <BtnShare red caption='Pledge Monthly' />
                 </div>
             </div>
         </div>
