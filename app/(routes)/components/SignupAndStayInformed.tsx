4b2e5849ec3a260d5bfe5087fa255424da8786c8
Baidar Gul <baidargul@outlook.com>
2024-03-13 20:07:44 +0500

    StayInformed Section

--- app/(routes)/components/SignupAndStayInformed.tsx
+++ app/(routes)/components/SignupAndStayInformed.tsx
@@ -1,3 +1,5 @@
+import BtnShare from '@/components/Site/Buttons/BtnShare'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 type Props = {}
@@ -9,8 +11,13 @@ const SignupAndStayInformed = (props: Props) => {
                 <div className="text-[32px] font-semibold">Sign Up, Stay Informed</div>
                 <div className="text-[16px] text-site-text-fundraiser_support_description">Get regular updates on all that we are doing to change lives</div>
             </div>
-            <div>
-                This
+            <div className='flex gap-4 p-4 justify-end items-center'>
+                <div className='w-full'>
+                    <Input placeholder='this' />
+                </div>
+                <div>
+                    <BtnShare red caption='Subscribe' textSize='text-lg' />
+                </div>
             </div>
         </div>
     )
