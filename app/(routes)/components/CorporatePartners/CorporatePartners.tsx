4b218fa3298f31a89f436549df0511ab8b4599f8
Baidar Gul <baidargul@outlook.com>
2024-03-13 20:28:16 +0500

    Coporate Partners section

--- app/(routes)/components/CorporatePartners/CorporatePartners.tsx
+++ app/(routes)/components/CorporatePartners/CorporatePartners.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
+
+const data = [
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/1.jpg",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/1.jpg",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/1.jpg",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/1.jpg",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/1.jpg",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+    {
+        name: "Adobe",
+        image: "/homepage/corporatepartners/2.png",
+    },
+   
+]
+
 const CorporatePartners = (props: Props) => {
+    const [visibleItems, setVisibleItems] = useState(data.slice(0, 8));
+    const [shouldFadeOut, setShouldFadeOut] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setShouldFadeOut(true);
+            setTimeout(() => {
+                setShouldFadeOut(false);
+                const nextIndex = (currentIndex + 8) % data.length;
+                setVisibleItems(data.slice(nextIndex, nextIndex + 8)); // Show next 8 items
+                setCurrentIndex(nextIndex);
+            }, 500); // Delay fade in by 500 milliseconds
+        }, 2000); // Wait for 2 seconds before fading out
+
+        return () => clearInterval(interval);
+    }, [currentIndex]); // Trigger effect whenever currentIndex changes
+
     return (
         <div className='flex flex-col justify-center items-center'>
             <div className="text-[48px] font-semibold text-site-text-tag_first_donor">
@@ -11,11 +96,15 @@ const CorporatePartners = (props: Props) => {
             <div className="text-[20px] text-site-text-fundraiser_support_description">
                 Trusted by the largest brands and corporations, and the most impactful foundations around the world
             </div>
-            <div>
-                
+            <div className={`flex gap-10 items-center transition-opacity duration-500 ${shouldFadeOut ? 'opacity-0' : 'opacity-100'}`}>
+                {visibleItems.map((company: any, index: number) => (
+                    <div key={company.name}>
+                        <Image src={company.image} width={130} height={130} alt='corporate' className='' />
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CorporatePartners
+export default CorporatePartners;
