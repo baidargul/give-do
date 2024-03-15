53fa332c2d225eda0215d6fea9b22d00d136fd92
Baidar Gul <baidargul@outlook.com>
2024-03-15 16:24:41 +0500

    Signup section

--- app/(routes)/components/SignupAndStayInformed.tsx
+++ app/(routes)/components/SignupAndStayInformed.tsx
@@ -6,16 +6,16 @@ type Props = {}
 
 const SignupAndStayInformed = (props: Props) => {
     return (
-        <div className="bg-white p-8 rounded-2xl drop-shadow-md grid grid-cols-2">
+        <div className="bg-white p-4 tablet:p-8 rounded-2xl drop-shadow-md tablet:grid tablet:grid-cols-2">
             <div className="">
-                <div className="text-[32px] font-semibold">Sign Up, Stay Informed</div>
-                <div className="text-[16px] text-site-text-fundraiser_support_description">Get regular updates on all that we are doing to change lives</div>
+                <div className="text-[1.5rem] tablet:text-[32px] font-semibold">Sign Up, Stay Informed</div>
+                <div className="text-[.9rem] mt-2 tablet:mt-0 tablet:text-[16px] text-site-text-fundraiser_support_description">Get regular updates on all that we are doing to change lives</div>
             </div>
-            <div className='flex gap-4 p-4 justify-end items-center'>
+            <div className='flex flex-col mt-4 tablet:mt-0 tablet:flex-row gap-4 p-4 justify-end items-center'>
                 <div className='w-full'>
                     <Input placeholder='Enter you Email ID' />
                 </div>
-                <div>
+                <div className='w-full'>
                     <BtnShare red caption='Subscribe' textSize='text-lg' />
                 </div>
             </div>
