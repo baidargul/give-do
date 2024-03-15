3272017ab3a2b868bfacefb67620735a6b67eaaa
Baidar Gul <baidargul@outlook.com>
2024-03-15 15:41:58 +0500

    trusted platform responsive

--- app/(routes)/components/mostTrustedPlatform/Grid.tsx
+++ app/(routes)/components/mostTrustedPlatform/Grid.tsx
@@ -4,24 +4,24 @@ type Props = {}
 
 const Grid = (props: Props) => {
     return (
-        <div className='relative mt-[300px]'>
+        <div className='relative mt-[200px] tablet:mt-[300px]'>
             <div className='flex justify-center'>
-                <div className='grid grid-cols-4 justify-items-center w-fit shadow-[0_0_6px_rgba(0,0,0,.12)] gap-16 p-5 relative z-0'>
+                <div className='grid grid-cols-2 justify-items-center text-center tablet:text-left tablet:grid-cols-4 w-fit shadow-[0_0_6px_rgba(0,0,0,.12)] gap-16 p-5 relative z-0'>
                     <div>
-                        <div className='text-[40px] font-semibold'>2.7M+</div>
-                        <div className='font-medium text-site-text-fundraiser_support_description'>Donors</div>
+                        <div className='text-[2rem] tablet:text-[40px] font-semibold'>2.7M+</div>
+                        <div className='font-medium text-[.9rem] text-site-text-fundraiser_support_description'>Donors</div>
                     </div>
                     <div>
-                        <div className='text-[40px] font-semibold'>15M+</div>
-                        <div className='font-medium text-site-text-fundraiser_support_description'>Lives Impacted</div>
+                        <div className='text-[2rem] tablet:text-[40px] font-semibold'>15M+</div>
+                        <div className='font-medium text-[.9rem] text-site-text-fundraiser_support_description'>Lives Impacted</div>
                     </div>
                     <div>
-                        <div className='text-[40px] font-semibold'>3000+</div>
-                        <div className='font-medium text-site-text-fundraiser_support_description'>Verified Non Profits</div>
+                        <div className='text-[2rem] tablet:text-[40px] font-semibold'>3000+</div>
+                        <div className='font-medium text-[.9rem] text-site-text-fundraiser_support_description'>Verified Non Profits</div>
                     </div>
                     <div>
-                        <div className='text-[40px] font-semibold'>300+</div>
-                        <div className='font-medium text-site-text-fundraiser_support_description'>Corporate Partners</div>
+                        <div className='text-[2rem] tablet:text-[40px] font-semibold'>300+</div>
+                        <div className='font-medium text-[.9rem] text-site-text-fundraiser_support_description'>Corporate Partners</div>
                     </div>
                 </div>
             </div>
--- app/(routes)/components/mostTrustedPlatform/Title.tsx
+++ app/(routes)/components/mostTrustedPlatform/Title.tsx
@@ -7,11 +7,11 @@ const Title = (props: Props) => {
     return (
         <div>
             <div className='relative flex justify-center'>
-                <div className='bg-gradient-to-b from-slate-50 to-transparent w-[386px] h-[386px] absolute rounded-full'></div>
-                <div className='absolute z-[1] top-[100px]'>
+                <div className='bg-gradient-to-b from-slate-50 to-transparent w-[200px] h-[286px] tablet:w-[386px] tablet:h-[386px] absolute rounded-full'></div>
+                <div className='absolute z-[1] top-[50px] tablet:top-[100px]'>
                     <SiteLogo size={70} />
                 </div>
-                <div className='text-[48px] font-semibold text-center absolute z-[1] top-[140px] w-[50%]'>
+                <div className='text-[1.5rem] font-extrabold tablet:text-[48px] tablet:font-semibold text-center absolute z-[1] top-[90px] tablet:top-[140px] tablet:w-[50%]'>
                     India’s most trusted online donation platform
                 </div>
             </div>
